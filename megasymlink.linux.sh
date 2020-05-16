03a1664f72243df07988e92830675c082e554230
danialmalik <danialmalik321@gmail.com>
2020-05-16 23:06:30 +0500

    remove subl config from mega

--- megasymlink.linux.sh
+++ megasymlink.linux.sh
@@ -6,6 +6,5 @@ mega_dir=~/MEGAsync
 
 # create symlinks
 sudo ln -sf  $mega_dir/synced-settings/vnote ~/.config/
-sudo ln -sf  $mega_dir/synced-settings/sublime-text-3 ~/.config/
-sudo ln -sf  $mega_dir/synced-settings/sublime-merge ~/.config/
-sudo ln -sf  $mega_dir/synced-settings/vnote ~/.config/
+# sudo ln -sf  $mega_dir/synced-settings/sublime-text-3 ~/.config/
+# sudo ln -sf  $mega_dir/synced-settings/sublime-merge ~/.config/
