e9e6ee53ccc77c47a884adee11461c270c7c5f54
danialmalik <danialmalik321@gmail.com>
2020-07-28 18:00:08 +0500

    Tue Jul 28 18:00:08 PKT 2020

--- symlink.mac.sh
+++ symlink.mac.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # variables
-dotfiles_dir=~/dotfiles
+dotfiles_dir=~/dotfiles/dotfiles
 
 
 # create symlinks
@@ -11,12 +11,6 @@ sudo ln -sf $dotfiles_dir/profile ~/.profile
 sudo ln -sf $dotfiles_dir/zshrc ~/.zshrc
 sudo ln -sf $dotfiles_dir/aliases ~/.bash_aliases
 sudo ln -sf $dotfiles_dir/aliases ~/.aliases
-
-# ssh
-sudo ln -sf $dotfiles_dir/ssh_config ~/.ssh/config
-
-# hosts
-sudo ln -sf $dotfiles_dir/hosts /etc/hosts
 
 # zsh theme
 sudo ln -sf $dotfiles/zsh_theme_powerline10k.zsh ~/.p10k.zsh
