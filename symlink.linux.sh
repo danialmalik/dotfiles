2b692e356abddb5d6d00c859d10f49cf1f92c000
danialmalik <danialmalik321@gmail.com>
2020-10-19 22:24:30 +0500

    Mon 19 Oct 2020 10:24:30 PM PKT

--- symlink.linux.sh
+++ symlink.linux.sh
@@ -5,15 +5,20 @@ dotfiles_dir=~/dotfiles/dotfiles
 dotfiles_secure_dir=~/dotfiles/secure_dotfiles
 
 # create dirs
-# mkdir ~/.config/terminator/
+mkdir ~/.config/terminator/
 
-# create symlinks
-ln -sf $dotfiles_dir/vimrc ~/.vimrc
+# shell symlinks
 ln -sf $dotfiles_dir/bashrc ~/.bashrc
 ln -sf $dotfiles_dir/profile ~/.profile
 ln -sf $dotfiles_dir/zshrc ~/.zshrc
 ln -sf $dotfiles_dir/aliases ~/.bash_aliases
 ln -sf $dotfiles_dir/aliases ~/.aliases
+
+# vim
+ln -sf $dotfiles_dir/vimrc ~/.vimrc
+
+# docker
+ln -sf $dotfiles_dir/docker-config.json ~/.docker/config.json
 
 #git
 ln -sf $dotfiles_dir/gitconfig ~/.gitconfig
@@ -29,4 +34,5 @@ sudo ln -sf $dotfiles_secure_dir/hosts /etc/hosts
 # https://github.com/bulletmark/libinput-gestures
 ln -sf $dotfiles_dir/config_files/libinput-gestures.conf ~/.config/libinput-gestures.conf
 
+# terminator
 ln -sf $dotfiles_dir/terminator_config ~/.config/terminator/config
