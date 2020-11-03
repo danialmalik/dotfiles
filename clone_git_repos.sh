d07f631eb6caa33f22517046fc64bdb7d1e54a2f
danialmalik <danialmalik321@gmail.com>
2020-11-03 18:45:47 +0500

    Tue Nov  3 06:45:47 PM PKT 2020

---
+++ clone_git_repos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# make directories
+mkdir -p $HOME/work/arbisoft/open-edx
+mkdir -p $HOME/work/personal
+
+# clone repos
+git clone git@github.com:danialmalik/cron-jobs.git $HOME/work/personal/cron-jobs
+git clone git@github.com:danialmalik/interview-questions.git $HOME/work/interview-questions
+git clone git@github.com:danialmalik/opnedx-private-files.git $HOME/work/arbisoft/open-edx/opnedx-private-files
