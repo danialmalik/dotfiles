99f2a2cdcbd3fa6862e2216353f1ea60fee622fb
danialmalik <danialmalik321@gmail.com>
2021-02-20 12:00:05 +0500

    Sat Feb 20 12:00:05 PM PKT 2021

--- megasymlink.linux.sh
+++ megasymlink.linux.sh
@@ -5,6 +5,6 @@ mega_dir=~/MEGAsync
 
 
 # create symlinks
-sudo ln -sf  $mega_dir/synced-settings/vnote ~/.config/
+# sudo ln -sf  $mega_dir/synced-settings/vnote ~/.config/
 # sudo ln -sf  $mega_dir/synced-settings/sublime-text-3 ~/.config/
 # sudo ln -sf  $mega_dir/synced-settings/sublime-merge ~/.config/
--- symlink.linux.sh
+++ symlink.linux.sh
@@ -2,27 +2,27 @@
 
 # variables
 dotfiles_dir=~/dotfiles/dotfiles
+config_files_dir=$dotfiles_dir/config_files
 dotfiles_secure_dir=~/dotfiles/secure_dotfiles
 
 # create dirs
 mkdir ~/.config/terminator/ 2>/dev/null
 
 # shell symlinks
-ln -sf $dotfiles_dir/bashrc ~/.bashrc
-ln -sf $dotfiles_dir/profile ~/.profile
-ln -sf $dotfiles_dir/zshrc ~/.zshrc
-ln -sf $dotfiles_dir/aliases ~/.bash_aliases
-ln -sf $dotfiles_dir/aliases ~/.aliases
+ln -sf $config_files_dir/bashrc ~/.bashrc
+ln -sf $config_files_dir/profile ~/.profile
+ln -sf $config_files_dir/zshrc ~/.zshrc
+ln -sf $config_files_dir/aliases ~/.bash_aliases
+ln -sf $config_files_dir/aliases ~/.aliases
 
 # vim
-ln -sf $dotfiles_dir/vimrc ~/.vimrc
+ln -sf $config_files_dir/vimrc ~/.vimrc
 
 # docker
-ln -sf $dotfiles_dir/docker-config.json ~/.docker/config.json
+ln -sf $config_files_dir/docker-config.json ~/.docker/config.json
 
 #git
-ln -sf $dotfiles_dir/gitconfig ~/.gitconfig
-ln -sf $dotfiles_dir/gitignore_global ~/.gitignore_global
+ln -sf $config_files_dir/gitconfig ~/.gitconfig
 
 # ssh
 ln -sf $dotfiles_secure_dir/ssh_config ~/.ssh/config
@@ -33,10 +33,10 @@ sudo ln -sf $dotfiles_secure_dir/hosts /etc/hosts
 # config files
 
 # https://github.com/bulletmark/libinput-gestures
-ln -sf $dotfiles_dir/config_files/libinput-gestures.conf ~/.config/libinput-gestures.conf
+ln -sf $config_files_dir/config_files/libinput_gestures/libinput-gestures.conf ~/.config/libinput-gestures.conf
 
 # terminator
-ln -sf $dotfiles_dir/terminator_config ~/.config/terminator/config
+ln -sf $config_files_dir/terminator_config ~/.config/terminator/config
 
 # pwerline10k theme
-ln -sf $dotfiles/zsh_theme_powerline10k.zsh ~/.p10k.zsh
+ln -sf $config_files_dir/zsh_theme_powerline10k.zsh ~/.p10k.zsh
