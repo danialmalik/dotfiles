804d6a51604fe2c8dbb52a84eb68f1d169f10689
danialmalik <danialmalik321@gmail.com>
2021-05-07 11:00:06 +0500

    Fri 07 May 2021 11:00:06 AM PKT

--- install_zsh_plugins.sh
+++ install_zsh_plugins.sh
@@ -6,6 +6,7 @@ cd $plugins_dir
 git clone https://github.com/djui/alias-tips.git
 git clone https://github.com/zsh-users/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zdharma/history-search-multi-word.git
 
 cd ..
 mv $plugins_dir/* ~/.oh-my-zsh/plugins
