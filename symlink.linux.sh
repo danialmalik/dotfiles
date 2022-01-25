a70185f592b84489d5b95d5aeefc638c04c67a5d
danialmalik <danialmalik321@gmail.com>
2022-01-25 23:00:11 +0500

    Tue Jan 25 11:00:11 PM PKT 2022

--- symlink.linux.sh
+++ symlink.linux.sh
@@ -1,39 +1,43 @@
 #!/bin/bash
 
 # variables
-dotfiles_dir=~/dotfiles/dotfiles
-config_files_dir=$dotfiles_dir/config_files
+DOTFILES_DIR=~/dotfiles/dotfiles
+CONFIG_FILES_DIR=$DOTFILES_DIR/config_files
+SERVICES_DIR=$DOTFILES_DIR/services
 
 # create dirs
 mkdir ~/.config/terminator/ 2>/dev/null
 mkdir ~/.config/nvim/ 2>/dev/null
 
 # shell symlinks
-ln -sf $config_files_dir/bashrc ~/.bashrc
-ln -sf $config_files_dir/profile ~/.profile
-ln -sf $config_files_dir/zshrc ~/.zshrc
-ln -sf $config_files_dir/aliases ~/.bash_aliases
-ln -sf $config_files_dir/aliases ~/.aliases
+ln -sf $CONFIG_FILES_DIR/bashrc ~/.bashrc
+ln -sf $CONFIG_FILES_DIR/profile ~/.profile
+ln -sf $CONFIG_FILES_DIR/zshrc ~/.zshrc
+ln -sf $CONFIG_FILES_DIR/aliases ~/.bash_aliases
+ln -sf $CONFIG_FILES_DIR/aliases ~/.aliases
 
 # vim
-ln -sf $config_files_dir/vimrc ~/.vimrc
-ln -sf $config_files_dir/nvim_init.vim ~/.config/nvim/init.vim
+ln -sf $CONFIG_FILES_DIR/vimrc ~/.vimrc
+ln -sf $CONFIG_FILES_DIR/nvim_init.vim ~/.config/nvim/init.vim
 
 #git
-ln -sf $config_files_dir/gitconfig ~/.gitconfig
+ln -sf $CONFIG_FILES_DIR/gitconfig ~/.gitconfig
 
 # config files
 
 # https://github.com/bulletmark/libinput-gestures
-ln -sf $config_files_dir/libinput_gestures/libinput-gestures.conf ~/.config/libinput-gestures.conf
+ln -sf $CONFIG_FILES_DIR/libinput_gestures/libinput-gestures.conf ~/.config/libinput-gestures.conf
 
 # terminator
-ln -sf $config_files_dir/terminator_config ~/.config/terminator/config
+ln -sf $CONFIG_FILES_DIR/terminator_config ~/.config/terminator/config
 
 # pwerline10k theme
-ln -sf $config_files_dir/zsh_theme_powerline10k.zsh ~/.p10k.zsh
+ln -sf $CONFIG_FILES_DIR/zsh_theme_powerline10k.zsh ~/.p10k.zsh
 
 # MPV media player
 mkdir -p ~/.config/mpv
-ln -sf $config_files_dir/mpv/input.conf ~/.config/mpv/input.conf
-ln -sf $config_files_dir/mpv/mpv.conf ~/.config/mpv/mpv.conf
+ln -sf $CONFIG_FILES_DIR/mpv/input.conf ~/.config/mpv/input.conf
+ln -sf $CONFIG_FILES_DIR/mpv/mpv.conf ~/.config/mpv/mpv.conf
+
+# Systemd services
+sudo ln -sf $SERVICES_DIR/battery-charge-threshold.service /etc/systemd/system/battery-charge-threshold.service
