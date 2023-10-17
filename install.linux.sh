e13a09e33a39e550a9982bdbd1aedf181349d70e
danialmalik <danialmalik321@gmail.com>
2023-10-17 15:00:14 +0500

    Tue Oct 17 03:00:14 PM PKT 2023

--- install.linux.sh
+++ install.linux.sh
@@ -103,8 +103,9 @@ echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://
 
 ########################## Disabled ##########################
 ### Brave
-# sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-# echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
 
 # uget
 # TODO: Add -y option ? so that doesn't ask for enter
