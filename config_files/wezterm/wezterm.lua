4e720f840a739b720e3fd96a145568a845e046fa
danialmalik <danialmalik321@gmail.com>
2023-11-03 12:00:09 +0500

    Fri Nov  3 12:00:09 PM PKT 2023

--- config_files/wezterm/wezterm.lua
+++ config_files/wezterm/wezterm.lua
@@ -1,5 +1,7 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local event_handlers = require 'wezterm_event_handlers'
+
 local act = wezterm.action
 
 -- This table will hold the configuration.
@@ -9,9 +11,8 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
-
 
 ------------------------ Appearance ------------------------------
 -- For example, changing the color scheme:
@@ -20,7 +21,7 @@ end
 config.color_scheme = 'iTerm2 Tango Dark'
 
 config.colors = {
-  background = 'black',
+    background = 'black'
 }
 
 -- config.window_background_image = '/path/to/wallpaper.jpg'
@@ -39,116 +40,98 @@ config.colors = {
 
 ------------------------ Configs --------------------------------
 
------------------------- Event handlers --------------------------
-wezterm.on('launch-1', function (window, pane1_1)
-  local pane1_2 = pane1_1:split {
-    direction = 'Bottom',
-    size = (1-0.333)
-  }
-  local pane1_3 = pane1_2:split {
-    direction = 'Bottom',
-    size = 0.5
-  }
-
-  pane1_1:split {
-    direction = 'Right',
-    size = 0.5
-  }
-
-  pane1_2:split {
-    direction = 'Right',
-    size = 0.5
-  }
-
-  pane1_3:split {
-    direction = 'Right',
-    size = 0.5
-  }
-
-
-end)
-
 ------------------------ Keybindings -----------------------------
 
-config.leader  = {
-  key = 'k',
-  mods = 'CTRL',
-  timeout_milliseconds = 3000
+config.leader = {
+    key = 'k',
+    mods = 'CTRL',
+    timeout_milliseconds = 3000
 }
 
-config.keys = {
-  {
+config.keys = {{
     key = 'p',
     mods = 'CTRL',
     action = act.ActivateCommandPalette
-  },
-
-
-  ------------- Switch Panes
-  {
+}, ------------- Switch Panes
+{
     key = 'LeftArrow',
     mods = 'SHIFT',
     action = act.ActivatePaneDirection 'Left'
-  },
-  {
+}, {
     key = 'RightArrow',
     mods = 'SHIFT',
     action = act.ActivatePaneDirection 'Right'
-  },
-  {
+}, {
     key = 'UpArrow',
     mods = 'SHIFT',
     action = act.ActivatePaneDirection 'Up'
-  },
-  {
+}, {
     key = 'DownArrow',
     mods = 'SHIFT',
     action = act.ActivatePaneDirection 'Down'
-  },
-
-  ------------- Pane size
-  {
+}, ------------- Pane size
+{
     key = 'LeftArrow',
     mods = 'ALT|SHIFT',
-    action = act.AdjustPaneSize { 'Left', 2 },
-  },
-  {
+    action = act.AdjustPaneSize {'Left', 2}
+}, {
     key = 'DownArrow',
     mods = 'ALT|SHIFT',
-    action = act.AdjustPaneSize { 'Down', 1 },
-  },
-  { key = 'UpArrow', mods = 'ALT|SHIFT', action = act.AdjustPaneSize { 'Up', 1 } },
-  {
+    action = act.AdjustPaneSize {'Down', 1}
+}, {
+    key = 'UpArrow',
+    mods = 'ALT|SHIFT',
+    action = act.AdjustPaneSize {'Up', 1}
+}, {
     key = 'RightArrow',
     mods = 'ALT|SHIFT',
-    action = act.AdjustPaneSize { 'Right', 2 },
-  },
-
-  -------------- Pane split
-  {
+    action = act.AdjustPaneSize {'Right', 2}
+}, -------------- Pane split
+{
     key = 'e',
     mods = 'CTRL|ALT',
-    action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }
-  },
-  {
+    action = act.SplitHorizontal {
+        domain = 'CurrentPaneDomain'
+    }
+}, {
     key = 'd',
     mods = 'CTRL|ALT',
-    action = act.SplitVertical { domain = 'CurrentPaneDomain' }
-  },
-  {
+    action = act.SplitVertical {
+        domain = 'CurrentPaneDomain'
+    }
+}, {
     key = 'X',
     mods = 'CTRL|SHIFT',
-    action = act.TogglePaneZoomState,
-  },
-
-  ----------- Custom Events
-  {
+    action = act.TogglePaneZoomState
+}, ----------- Custom Events
+{
     key = '1',
     mods = 'LEADER|CTRL',
-    action = act.EmitEvent 'launch-1'
-  }
-}
+    action = act.EmitEvent 'launch-tier3'
+}, {
+    key = '2',
+    mods = 'LEADER|CTRL',
+    action = act.EmitEvent 'launch-unhaggle'
+}, {
+    key = '3',
+    mods = 'LEADER|CTRL',
+    action = act.EmitEvent 'launch-asc'
+}}
 
+---------------------- Change tab switch key
+for i = 1, 8 do
+    -- CTRL + number to activate that tab
+    table.insert(config.keys, {
+        key = tostring(i),
+        mods = 'CTRL',
+        action = act.ActivateTab(i - 1)
+    })
+    -- F1 through F8 to activate that tab
+    table.insert(config.keys, {
+        key = 'F' .. tostring(i),
+        action = act.ActivateTab(i - 1)
+    })
+end
 
 -- and finally, return the configuration to wezterm
 return config
