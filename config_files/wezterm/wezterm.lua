d294407b16911e388e50753d9960e8faa181244d
danialmalik <danialmalik321@gmail.com>
2023-11-05 09:00:08 +0500

    Sun Nov  5 09:00:08 AM PKT 2023

--- config_files/wezterm/wezterm.lua
+++ config_files/wezterm/wezterm.lua
@@ -10,19 +10,47 @@ local config = {}
 ------------------------- Initialize -----------------------------
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
-if wezterm.config_builder then
-    config = wezterm.config_builder()
-end
+if wezterm.config_builder then config = wezterm.config_builder() end
 
 ------------------------ Appearance ------------------------------
--- For example, changing the color scheme:
--- config.color_scheme = 'FirefoxDev'
--- config.color_scheme = 'Gnometerm (terminal.sexy)'
-config.color_scheme = 'iTerm2 Tango Dark'
+---cycle through builtin dark schemes in dark mode,
+---and through light schemes in light mode
+local function themeCycler(window, _)
+    local allSchemes = wezterm.color.get_builtin_schemes()
+    local currentMode = wezterm.gui.get_appearance()
+    local currentScheme = window:effective_config().color_scheme
+    local darkSchemes = {}
+    local lightSchemes = {}
 
-config.colors = {
-    background = 'black'
-}
+    for name, scheme in pairs(allSchemes) do
+        local bg = wezterm.color.parse(scheme.background) -- parse into a color object
+        ---@diagnostic disable-next-line: unused-local
+        local h, s, l, a = bg:hsla() -- and extract HSLA information
+        if l < 0.4 then
+            table.insert(darkSchemes, name)
+        else
+            table.insert(lightSchemes, name)
+        end
+    end
+    local schemesToSearch = currentMode:find("Dark") and darkSchemes or lightSchemes
+
+    for i = 1, #schemesToSearch, 1 do
+        if schemesToSearch[i] == currentScheme then
+            local overrides = window:get_config_overrides() or {}
+            overrides.color_scheme = schemesToSearch[i + 1]
+            wezterm.log_info("Switched to: " .. schemesToSearch[i + 1])
+            window:set_config_overrides(overrides)
+            return
+        end
+    end
+end
+
+-- For example, changing the color scheme:
+-- Later This Evening
+config.color_scheme = 'Builtin Dark'
+config.color_scheme = 'Kolorit'
+
+-- config.colors = {background = 'black'}
 
 -- config.window_background_image = '/path/to/wallpaper.jpg'
 
@@ -42,81 +70,58 @@ config.colors = {
 
 ------------------------ Keybindings -----------------------------
 
-config.leader = {
-    key = 'k',
-    mods = 'CTRL',
-    timeout_milliseconds = 3000
-}
+config.leader = {key = 'k', mods = 'CTRL', timeout_milliseconds = 3000}
 
-config.keys = {{
-    key = 'p',
-    mods = 'CTRL',
-    action = act.ActivateCommandPalette
-}, ------------- Switch Panes
-{
-    key = 'LeftArrow',
-    mods = 'SHIFT',
-    action = act.ActivatePaneDirection 'Left'
-}, {
-    key = 'RightArrow',
-    mods = 'SHIFT',
-    action = act.ActivatePaneDirection 'Right'
-}, {
-    key = 'UpArrow',
-    mods = 'SHIFT',
-    action = act.ActivatePaneDirection 'Up'
-}, {
-    key = 'DownArrow',
-    mods = 'SHIFT',
-    action = act.ActivatePaneDirection 'Down'
-}, ------------- Pane size
-{
-    key = 'LeftArrow',
-    mods = 'ALT|SHIFT',
-    action = act.AdjustPaneSize {'Left', 2}
-}, {
-    key = 'DownArrow',
-    mods = 'ALT|SHIFT',
-    action = act.AdjustPaneSize {'Down', 1}
-}, {
-    key = 'UpArrow',
-    mods = 'ALT|SHIFT',
-    action = act.AdjustPaneSize {'Up', 1}
-}, {
-    key = 'RightArrow',
-    mods = 'ALT|SHIFT',
-    action = act.AdjustPaneSize {'Right', 2}
-}, -------------- Pane split
-{
-    key = 'e',
-    mods = 'CTRL|ALT',
-    action = act.SplitHorizontal {
-        domain = 'CurrentPaneDomain'
-    }
-}, {
-    key = 'd',
-    mods = 'CTRL|ALT',
-    action = act.SplitVertical {
-        domain = 'CurrentPaneDomain'
-    }
-}, {
-    key = 'X',
-    mods = 'CTRL|SHIFT',
-    action = act.TogglePaneZoomState
-}, ----------- Custom Events
-{
-    key = '1',
-    mods = 'LEADER|CTRL',
-    action = act.EmitEvent 'launch-tier3'
-}, {
-    key = '2',
-    mods = 'LEADER|CTRL',
-    action = act.EmitEvent 'launch-unhaggle'
-}, {
-    key = '3',
-    mods = 'LEADER|CTRL',
-    action = act.EmitEvent 'launch-asc'
-}}
+config.keys = {
+    {key = 'p', mods = 'CTRL', action = act.ActivateCommandPalette},
+    ------------- Appearance related
+    { key = "t", mods = "ALT", action = wezterm.action_callback(themeCycler) },
+
+    ------------- Switch Panes
+    {
+        key = 'LeftArrow',
+        mods = 'SHIFT',
+        action = act.ActivatePaneDirection 'Left'
+    }, {
+        key = 'RightArrow',
+        mods = 'SHIFT',
+        action = act.ActivatePaneDirection 'Right'
+    },
+    {key = 'UpArrow', mods = 'SHIFT', action = act.ActivatePaneDirection 'Up'},
+    {
+        key = 'DownArrow',
+        mods = 'SHIFT',
+        action = act.ActivatePaneDirection 'Down'
+    }, ------------- Pane size
+    {
+        key = 'LeftArrow',
+        mods = 'ALT|SHIFT',
+        action = act.AdjustPaneSize {'Left', 2}
+    }, {
+        key = 'DownArrow',
+        mods = 'ALT|SHIFT',
+        action = act.AdjustPaneSize {'Down', 1}
+    },
+    {key = 'UpArrow', mods = 'ALT|SHIFT', action = act.AdjustPaneSize {'Up', 1}},
+    {
+        key = 'RightArrow',
+        mods = 'ALT|SHIFT',
+        action = act.AdjustPaneSize {'Right', 2}
+    }, -------------- Pane split
+    {
+        key = 'e',
+        mods = 'CTRL|ALT',
+        action = act.SplitHorizontal {domain = 'CurrentPaneDomain'}
+    }, {
+        key = 'd',
+        mods = 'CTRL|ALT',
+        action = act.SplitVertical {domain = 'CurrentPaneDomain'}
+    }, {key = 'X', mods = 'CTRL|SHIFT', action = act.TogglePaneZoomState},
+    ----------- Custom Events
+    {key = '1', mods = 'LEADER|CTRL', action = act.EmitEvent 'launch-tier3'},
+    {key = '2', mods = 'LEADER|CTRL', action = act.EmitEvent 'launch-unhaggle'},
+    {key = '3', mods = 'LEADER|CTRL', action = act.EmitEvent 'launch-asc'}
+}
 
 ---------------------- Change tab switch key
 for i = 1, 8 do
@@ -127,10 +132,8 @@ for i = 1, 8 do
         action = act.ActivateTab(i - 1)
     })
     -- F1 through F8 to activate that tab
-    table.insert(config.keys, {
-        key = 'F' .. tostring(i),
-        action = act.ActivateTab(i - 1)
-    })
+    table.insert(config.keys,
+                 {key = 'F' .. tostring(i), action = act.ActivateTab(i - 1)})
 end
 
 -- and finally, return the configuration to wezterm
