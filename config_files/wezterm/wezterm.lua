ba6a8f36ddba545c64443842faa01460d6354e77
danialmalik <danialmalik321@gmail.com>
2024-01-29 16:00:08 +0500

    Mon Jan 29 04:00:08 PM PKT 2024

--- config_files/wezterm/wezterm.lua
+++ config_files/wezterm/wezterm.lua
@@ -75,6 +75,8 @@ config.window_background_image_hsb = {
 }
 
 ------------------------ Configs --------------------------------
+config.check_for_updates = true
+config.check_for_updates_interval_seconds = 86400 -- 1 day
 
 ------------------------ Keybindings -----------------------------
 
